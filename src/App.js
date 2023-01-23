ad311575c3ed3ca5ed276e46c84de2d947247010
Alisuf <alisufiyan47@gmail.com>
2023-01-23 16:30:58 +0530

    api call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+const [data, setData]= useState(null);
+useEffect(()=>{  
+fetch("https://reqres.in/api/users/2")
+.then((resp)=>resp.json())
+.then((resp)=>{
+  setData(resp)
+})
+},[])
+console.log(data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{marginTop:'2em', fontWeight:'bold'}}>
+    Api Call:
+    <div>Id:{data.data.id}</div>
+    <div>Email:{data.data.email}</div>
+    <div>First Name:{data.data.first_name}</div>
     </div>
   );
 }
